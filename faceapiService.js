0d854feb2ad88b2bcaaf1e23f6dda8a15b019547
AmrSaleh2234 <amrsaleh1001@gmail.com>
2023-05-18 15:18:15 +0300

    f

--- faceapiService.js
+++ faceapiService.js
@@ -1,8 +1,9 @@
 const path = require("path");
+// import * as canvas from 'canvas';
+// const { Canvas, Image, ImageData } = canvas
 
-
-// const faceapi = require("@vladmandic/face-api/dist/face-api.node.js");
 const faceapi = require("./face-api.min")
+const {promises} = require("fs");
 const modelPathRoot = "./models";
 
 let optionsSSDMobileNet;
@@ -25,9 +26,27 @@ async function main(file) {
 
     console.log("Loading FaceAPI models");
     const modelPath = path.join(__dirname, modelPathRoot);
+    await faceapi.nets.faceRecognitionNet.loadFromDisk(modelPath);
+    await faceapi.nets.faceLandmark68Net.loadFromDisk(modelPath);
     await faceapi.nets.ssdMobilenetv1.loadFromDisk(modelPath);
 
 
+    // const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']c
+    //
+    // var fs = require('fs');
+    // var imgBase64= "data:image/jpeg;base64,"+fs.readFileSync('./upload/2.jpg', 'base64')
+    // const cheerio = require('cheerio');
+    // const $ = cheerio.load(`<img id="img" src="${imgBase64}" >`);
+    const request = require('request');
+
+    request('http://127.0.0.1:3000/', async function (error, response, body) {
+
+        // const detections = await faceapi.detectAllFaces(body).withFaceLandmarks().withFaceDescriptors()
+        console.log(typeof body)
+    });
+    // console.log( )
+
+    // console.log(imgBase64)
 
     return true;
 }
--- index.js
+++ index.js
@@ -8,15 +8,17 @@ app.use(fileUpload());
 
 app.post("/upload", async (req, res) => {
     const { file } = req.files;
-
+    file.mv(__dirname + '/upload/' + file.name);
     const result = await faceApiService.detect(file.data);
 
-    console.log(file);
-    res.send("Successfile upload");
+    // console.log(file);
+    res.send(result);
 
 });
 
-
+app.get('/',async (req,res)=>{
+    res.sendFile(__dirname +'/main.html');
+})
 
 app.listen(port, () => {
     console.log("Server started on port" + port);
